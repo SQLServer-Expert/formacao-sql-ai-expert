38b88c498035364c1b5043d50386565823e4dcae
landry_SQL2025 <proflandry.sqlexpert@gmail.com>
2026-09-06 13:02:20 -0300

    Atualizar

--- 05-Curso-Azure-IA/Mod03-Microsoft-Foundry/A07 RAG.sql
+++ 05-Curso-Azure-IA/Mod03-Microsoft-Foundry/A07 RAG.sql
@@ -26,14 +26,12 @@ CREATE or ALTER PROC dbo.ia_RAG_Foundry
   Chamada:
   EXEC dbo.ia_RAG_Foundry 
   @Pergunta = N'Como e quando utilizar o comando DBCC CHECKDB no SQL Server?',
-  --@Pergunta = N'O que é e como utilizar Tabelas Temporais no SQL Server',
   @TopDocumentos = 8,
   @vModelo = N'gpt-5.6-luna',
   @vTemp = N'1'
 
-  https://sqlserver-expert.hashnode.dev/tabela-temporal-no-sql-server-parte-1-introducao-ao-conceito
-  https://sqlserver-expert.hashnode.dev/tabela-temporal-no-sql-server-parte-2-consultando-dados-historicos
-  https://sqlserver-expert.hashnode.dev/tabela-temporal-no-sql-server-parte-3-boas-praticas-e-limitacoes
+  https://sqlserver-expert.hashnode.dev/meu-banco-corrompeu-no-sql-server-e-agora
+  https://sqlserver-expert.hashnode.dev/como-detectar-a-ausencia-de-backups-no-sql-server
 ==============================================================================*/
 @Pergunta nvarchar(2000) = N'Como usar DBCC CHECKDB no SQL Server?',
 @TopDocumentos int = 8,
